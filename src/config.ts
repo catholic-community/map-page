b438829a9862480fafaddf675f23ee508fce3ce3
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-01-25 14:56:28 -0300

    Update SITE_URL in config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export let SITE_URL: string
 
 switch (process.env.NEXT_PUBLIC_ENVIRONMENT) {
 	case 'production':
-		SITE_URL = 'https://www.catholiccommunity.social'
+		SITE_URL = 'https://catholiccommunity.social'
 		break
 	default:
 		SITE_URL = 'http://localhost:3000'
