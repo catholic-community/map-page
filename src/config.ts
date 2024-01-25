9d2d0b9a98ecb7b10dcec0a42fd7842cf4c36f70
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-01-25 14:31:00 -0300

    Add config file with SITE_URL

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export let SITE_URL: string
+
+switch (process.env.NEXT_PUBLIC_ENVIRONMENT) {
+	case 'production':
+		SITE_URL = 'https://www.catholiccommunity.social'
+		break
+	default:
+		SITE_URL = 'http://localhost:3000'
+		break
+}
